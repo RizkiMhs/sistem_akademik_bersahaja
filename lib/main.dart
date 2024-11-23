f14b15aeb482836bd7963275886a89a07584a4cb
RizkiMhs <rizki.230180100@mhs.unimal.ac.id>
2024-11-23 19:35:19 +0700

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,14 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_application_1/bahan%20kuliah/bahankuliah.dart';
-import 'package:flutter_application_1/bahan%20kuliah/detailbahan.dart';
-import 'package:flutter_application_1/infoakun/infoakun.dart';
-import 'package:flutter_application_1/khs/info_khs.dart';
-import 'package:flutter_application_1/khs/khs1.dart';
-import 'package:flutter_application_1/krs/krs.dart';
-import 'package:flutter_application_1/krs/pilihmk.dart';
-import 'package:flutter_application_1/saran/saran.dart';
-import 'package:flutter_application_1/tagihan/riwayat.dart';
-import 'package:flutter_application_1/tagihan/tagihan.dart';
-import 'package:flutter_application_1/trNilai/transkipnilai.dart';
-import 'package:flutter_application_1/views/homepage.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter_application_1/views/login2.dart';
+import 'firebase_options.dart';
 import 'package:flutter_application_1/views/tes.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
@@ -29,7 +24,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home:  SplashScreen(),
+      home: LoginTwo(),
     );
   }
 }
